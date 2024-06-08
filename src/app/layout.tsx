1054f9b7b74234e83acc50757bf3b839b535e23c
Tejash Patel <tejpat98@gmail.com>
2024-06-09 00:00:45 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'rPlaces',
-  description: 'A clone of Reddits famous /r/places game.',
-}
+  title: "rPlace",
+  description: "A clone of Reddits famous /r/place game.",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
