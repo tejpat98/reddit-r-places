254c720e0fa2d09212db81e40abfca21545792a9
Tejash Patel <tejpat98@gmail.com>
2025-02-01 11:38:56 +0000

    Trying to fix zooming

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -42,7 +42,7 @@ const setTransform = () => {
 
   canvasElement!.style.transform = `translate(${canvas.panOffsetX}px, ${canvas.panOffsetY}px) scale(${zoomScale})`;
 
-  selectorImg!.style.transform = `translate(${totalSelectorOffset.dx}px, ${totalSelectorOffset.dy}px) scale(${zoomScale * 1.2})`;
+  selectorImg!.style.transform = `translate(${totalSelectorOffset.dx}px, ${totalSelectorOffset.dy}px) scale(${(zoomScale * 1.2) / window.devicePixelRatio})`;
 };
 const mouseSCROLL = (e: WheelEvent) => {
   e.preventDefault();
